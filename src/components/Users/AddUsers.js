3b0a4ce076e3d5bda7a828eecd0614bcb5bc5609
Apekshit <apekshit33@gmail.com>
2022-10-31 14:14:45 +0530

    Added logic for error modal

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -8,14 +8,23 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        messaage: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
 
     if (enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        messaage: "Please enter a valid age (> 0).",
+      });
       return;
     }
 
@@ -32,9 +41,19 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.messaage}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="">Username</label>
